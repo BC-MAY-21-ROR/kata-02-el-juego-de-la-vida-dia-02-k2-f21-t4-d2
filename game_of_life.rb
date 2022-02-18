cc949199a125a02d3c6790fb5f1f4583270e5755
Javier Ruiz <javier.ruiz.cadena@gmail.com>
2022-02-17 18:55:12 -0600

    Challenge was completed
    Co-authored-by: alanRazoMen <alanRazoMen@users.noreply.github.com>
    Co-authored-by: DiegoSteve <DiegoSteve@users.noreply.github.com>
    Co-authored-by: Elías U. Rodríguez <EliasUR@users.noreply.github.com>

---
+++ game_of_life.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative 'cell'
+require 'matrix'
+
+# Game of life algorithm
+class Grid
+  def initialize(rows, columns)
+    @x = rows
+    @y = columns
+    @m = Matrix.build(@x, @y) { Cell.new }
+  end
+
+  def show_matrix
+    @x.times do |x|
+      @y.times do |y|
+        print @m[x, y].print_body
+      end
+      puts ''
+    end
+  end
+
+  def new_generation
+    @x.times do |x|
+      @y.times do |y|
+        @m[x, y].update_state
+      end
+    end
+  end
+
+  def analyze_cells
+    @x.times do |x|
+      @y.times do |y|
+        @m[x, y].neighbours_count(@m, x, y)
+      end
+    end
+  end
+end
+
+grid = Grid.new(4, 8)
+grid.show_matrix
+grid.analyze_cells
+grid.new_generation
+puts 'generation 2'
+grid.show_matrix
